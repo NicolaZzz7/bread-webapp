4c3a74dd92fb8a51a642d8e8ff9ee5d0afecc4a7
NicolaZzz <nicolazzz@mail.ru>
2025-09-27 19:48:21 +0800

    Исправление логики корзины, добавки и уведомления дубль4

--- public/script.js
+++ public/script.js
@@ -82,6 +82,8 @@ function openProductModal(productId) {
   if (!quantities[productId]) quantities[productId] = {};
   if (!addonsSelected[productId]) addonsSelected[productId] = {};
 
+  const availableWeights = getAvailableWeights(product);
+  const hasAddons = product.addons && product.addons !== '';
   // Загрузка актуального количества из корзины
   availableWeights.forEach(({weight}) => {
     const existingItem = cart.find(item => item.id === productId && item.weight === weight);
@@ -89,8 +91,7 @@ function openProductModal(productId) {
     addonsSelected[productId][weight] = existingItem ? existingItem.hasAddons : false;
   });
 
-  const availableWeights = getAvailableWeights(product);
-  const hasAddons = product.addons && product.addons !== '';
+
 
   const modalHTML = `
     <div class="modal-content">
